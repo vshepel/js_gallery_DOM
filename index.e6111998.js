8f1ef9681c29a1c6e00a249033f27f4fea4dfb77
Volodymyr Shepel <web.shepel@gmail.com>
2024-04-28 20:34:36 +0300

    make build

---
+++ index.e6111998.js
@@ -0,0 +1,2 @@
+var e=document.getElementById("largeImg");document.getElementById("thumbs").addEventListener("click",function(t){t.preventDefault();var r=t.target.closest(".gallery__thumb");if(r){var a=r.closest("a");a&&(e.src=a.href)}});
+//# sourceMappingURL=index.e6111998.js.map
